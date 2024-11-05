f7a282e11b07329b1d48295aabbda52e63ce15a4
Marios Siati (The05) <85685549+marios-siatis@users.noreply.github.com>
2024-11-05 21:56:09 +0000

    refactors Letter filter for extensibility

--- src/Calastone.TextFilter/Filters/LetterFilter.cs
+++ src/Calastone.TextFilter/Filters/LetterFilter.cs
@@ -2,9 +2,11 @@ namespace Calastone.TextFilter.Filters;
 
 public class LetterFilter : WordFilter
 {
+    private readonly char _letter = 't';
+
     public override string Apply(string word)
     {
-        return word.ToLower().Contains('t') ? string.Empty : word;
+        return word.ToLower().Contains(_letter) ? string.Empty : word;
     }
 
     public override bool IsEnabled() => true;
